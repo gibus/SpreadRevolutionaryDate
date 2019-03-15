b733a7078c448a98ffff5389beee83bdcf999d89
gibus <gibus@april.org>
2019-03-15 23:57:31 +0100

    Delete linefeed before wikipedia link

--- lib/App/SpreadRevolutionaryDate.pm
+++ lib/App/SpreadRevolutionaryDate.pm
@@ -106,7 +106,7 @@ sub spread {
   my $revolutionary = $self->config->acab ?
       DateTime::Calendar::FrenchRevolutionary->now->set(hour => 1, minute => 31, second => 20, locale => $locale)
     : DateTime::Calendar::FrenchRevolutionary->now;
-  my $msg = $locale eq 'fr' ? $revolutionary->strftime("Nous sommes le %A, %d %B de l'An %EY (%Y) de la Révolution, %Ej, il est %T!\nhttps://fr.wikipedia.org/wiki/!!%Oj!!") : $revolutionary->strftime("We are %A, %d %B of Revolution Year %EY (%Y), %Ej, it is %T!");
+  my $msg = $locale eq 'fr' ? $revolutionary->strftime("Nous sommes le %A, %d %B de l'An %EY (%Y) de la Révolution, %Ej, il est %T! https://fr.wikipedia.org/wiki/!!%Oj!!") : $revolutionary->strftime("We are %A, %d %B of Revolution Year %EY (%Y), %Ej, it is %T!");
   $msg =~ s/!!([^!]+)!!/uri_escape($1)/e;
 
   $self->twitter->spread($msg) if $self->config->twitter;
