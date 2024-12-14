0dc1a49a54c298ac1acb007dfab692ddd0637415
gibus <gibus@easter-eggs.com>
2024-12-14 14:56:48 +0100

    Post website card embed to bluesky if url is posted

--- lib/App/SpreadRevolutionaryDate/BlueskyLite.pm
+++ lib/App/SpreadRevolutionaryDate/BlueskyLite.pm
@@ -17,6 +17,7 @@ use LWP::UserAgent;
 use DateTime;
 use JSON qw(encode_json decode_json);
 use URI;
+use Encode qw(decode_utf8);
 
 use namespace::autoclean;
 
@@ -41,7 +42,6 @@ sub new {
   my %self;
   $self{ua} = LWP::UserAgent->new(env_proxy => 1, timeout => 10, agent =>'App::SpreadRevolutionaryDate bot');
   $self{ua}->default_header('Accept' => 'application/json');
-  $self{ua}->default_header('Content-Type' => 'application/json');
 
   my %payload = (
     identifier => $args{'identifier'},
@@ -49,11 +49,10 @@ sub new {
   );
   my $json = encode_json(\%payload);
 
-  my $response = $self{ua}->post(
-    'https://bsky.social/xrpc/com.atproto.server.createSession',
-    Content_Type => 'application/json',
-    Content => $json
-  );
+  my $req = HTTP::Request->new('POST', 'https://bsky.social/xrpc/com.atproto.server.createSession');
+  $req->header('Content-Type' => 'application/json');
+  $req->content($json);
+  my $response = $self{ua}->request($req);
 
   if ($response->is_success) {
     my $content = decode_json($response->decoded_content);
@@ -76,28 +75,32 @@ Creates a Bluesky post.
 sub create_post {
   my ($self, $text) = @_;
 
-  my $facets = $self->_generate_facets($text);
+  my ($facets, $embed) = $self->_generate_facets($text);
   my $json = encode_json({
     repo => $self->{did},
     collection => 'app.bsky.feed.post',
     record => {
       text => $text,
       facets => $facets,
+      ($embed ?
+        (embed => $embed)
+        : ()
+      ),
       createdAt => DateTime->now->iso8601 . 'Z',
     },
   });
-  my $response = $self->{ua}->post(
-    "https://bsky.social/xrpc/com.atproto.repo.createRecord",
-    Content_Type => 'application/json',
-    Content => $json,
-  );
-  $response
+  my $req = HTTP::Request->new('POST', 'https://bsky.social/xrpc/com.atproto.repo.createRecord');
+  $req->header('Content-Type' => 'application/json');
+  $req->content($json);
+  my $response = $self->{ua}->request($req);
+  return $response
 }
 
 sub _generate_facets {
   my $self = shift;
   my $text = shift || return [];
-  my $output = [];
+  my $facets = [];
+  my $embed;
   my $pos = 0;
   foreach my $w (split /\s+/, $text) {
     my ($type, $attrib, $val);
@@ -111,20 +114,80 @@ sub _generate_facets {
       utf8::encode(my $text_bytes = $text);
       $pos = index($text_bytes, $w, $pos);
       my $end = $pos + length($w);
-      push @$output, {
-        features => [{
-          '$type' => $type,
-          uri     => $val,
-        }],
+
+      push @$facets, {
+        features => [
+          {
+            '$type' => $type,
+            uri     => $val,
+          },
+        ],
         index => {
           byteStart => $pos,
           byteEnd   => $end,
         },
       };
+
+      unless ($embed) {
+        my $card = $self->_fetch_embed_url_card($val);
+        if ($card) {
+          $embed = {
+                '$type'    => 'app.bsky.embed.external',
+                'external' => $card,
+          };
+        }
+      }
+
       $pos = $end;
     }
   }
-  $output
+  return ($facets, $embed);
+}
+
+sub _fetch_embed_url_card {
+  my $self = shift;
+  my $url = shift || return;
+
+  my $card = {uri => $url};
+
+  my $ua = LWP::UserAgent->new(env_proxy => 1, timeout => 10, agent =>'App::SpreadRevolutionaryDate bot');
+  my $response = $ua->get($url);
+  return unless $response->is_success;
+  my $content = $response->content;
+  return unless $content;
+
+  if ($content =~ /<meta property="og:title" content="([^"]+)"/) {
+    my $title = $1;
+    ($card->{title}) = decode_utf8($title);
+  } else {
+    $card->{title} = '';
+  }
+  if ($content =~ /<meta property="og:description" content="([^"]+)"/) {
+    my $description = $1;
+    ($card->{description}) = decode_utf8($description);
+  } else {
+    $card->{description} = '';
+  }
+  if ($content =~ /<meta property="og:image" content="([^"]+)"/) {
+    my $img_url = $1;
+    unless ($img_url =~ m!://!) {
+      $url = "$url/" unless $url =~ m!/$!;
+      $img_url = $url . $img_url;
+    }
+    my $img_response = $ua->get($img_url);
+    return unless $img_response->is_success;
+
+    my $blob_req = HTTP::Request->new('POST', 'https://bsky.social/xrpc/com.atproto.repo.uploadBlob');
+    $blob_req->header('Content-Type' => $img_response->header('Content-Type'));
+    $blob_req->content($img_response->content);
+    my $blob_response = $self->{ua}->request($blob_req);
+    return unless $blob_response->is_success;
+
+    my $blob_content = decode_json($blob_response->decoded_content);
+    ($card->{thumb}) = $blob_content->{blob};
+  }
+
+  return $card;
 }
 
 =head1 SEE ALSO
