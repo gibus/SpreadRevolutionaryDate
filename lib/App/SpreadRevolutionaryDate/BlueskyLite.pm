c2c5b92a475a4fff2b564a2bbf242729a721ba37
gibus <gibus@easter-eggs.com>
2025-03-06 22:34:18 +0100

    Fix file declarations

--- lib/App/SpreadRevolutionaryDate/BlueskyLite.pm
+++ lib/App/SpreadRevolutionaryDate/BlueskyLite.pm
@@ -1,17 +1,8 @@
-#
-# This file is part of App-SpreadRevolutionaryDate
-#
-# This software is Copyright (c) 2019-2024 by Gérald Sédrati.
-#
-# This is free software, licensed under:
-#
-#   The GNU General Public License, Version 3, June 2007
-#
 use 5.014;
 use utf8;
 package App::SpreadRevolutionaryDate::BlueskyLite;
-$App::SpreadRevolutionaryDate::BlueskyLite::VERSION = '0.34';
-# ABSTRACT: .
+
+# ABSTRACT: Simple Class to post status to BlueSky.
 
 use LWP::UserAgent;
 use DateTime;
